fbdc555f4d31c3536b1edebb459323552776ce63
Robles9732 <marcosrobles@gmail.com>
2026-05-24 18:51:38 -0300

    Conexão com o banco de dados

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,12 +1,13 @@
 from http import HTTPStatus
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from fast_zero.schemas import Message, UserPublic, UserSchema, UserDB, UserList
+from fast_zero.schemas import Message, UserPublic, UserSchema, UserList
+from sqlalchemy.exc import IntegrityError
+
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserPublic, UserSchema, UserDB, UserList
-
 
 
 app = FastAPI()
@@ -26,9 +27,13 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     )
     if db_user:
         if db_user.username == user.username:
-            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Username already exists')
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT, detail='Username already exists'
+            )
         elif db_user.email == user.email:
-            raise HTTPException(status_code=HTTPStatus.CONFLICT, detail='Email already exists')
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT, detail='Email already exists'
+            )
 
     db_user = User(
         username=user.username,
@@ -42,22 +47,39 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def list_users():
-    return {'users': database}
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+
+    return {'users': users}
 
 
 @app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
-    return user_with_id
+    try:
+        db_user.username = user.username
+        db_user.email = user.email
+        db_user.password = user.password
+        session.commit()
+        session.refresh(db_user)
+        return db_user
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail='Username or Email already exists'
+        )
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
-    del database[user_id - 1]
+    session.delete(db_user)
+    session.commit()
     return {'message': 'User deleted successfully'}
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,36 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from contextlib import contextmanager
-from datetime import datetime
+from sqlalchemy.pool import StaticPool
+
 from fast_zero.app import app
-from fast_zero.models import table_registry
+from fast_zero.database import get_session
+from fast_zero.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
@@ -27,18 +42,29 @@ def session():
 
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
-
-    def fake_time_hook(mapper, connection, target):
+    def fake_time_handler(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
 
-    event.listen(model, 'before_insert', fake_time_hook)
+    event.listen(model, 'before_insert', fake_time_handler)
 
     yield time
 
-    event.remove(model, 'before_insert', fake_time_hook)
+    event.remove(model, 'before_insert', fake_time_handler)
 
 
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
